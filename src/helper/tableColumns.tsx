b203a4fc9f30a6f418e883215bd3c466ac1b248d
LKPShubhamkar <shubhamkar@lkpsec.com>
2025-02-28 16:47:09 +0530

    registration details UI  page added

--- src/helper/tableColumns.tsx
+++ src/helper/tableColumns.tsx
@@ -207,25 +207,26 @@ export const getAccountDetails: GridColDef[] = [
 export const cardDetails = [
   {
     id: 1,
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN14vLyQvBxXJf60w_-n1dPFhqV-W6bjWwbw&s",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN14vLyQvBxXJf60w_-n1dPFhqV-W6bjWwbw&s",
     title: "LKP Brochure",
-    pdfUrl: " "
+    pdfUrl: " ",
   },
   {
     id: 2,
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6ibwvGamxlftRJQz6fFT3h7HH-aKKwxWmAQ&s",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6ibwvGamxlftRJQz6fFT3h7HH-aKKwxWmAQ&s",
     title: "SPIP Brochure",
-    pdfUrl: "../../../public/JavaScript-Core.pdf"
+    pdfUrl: "../../../public/JavaScript-Core.pdf",
   },
   {
     id: 3,
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN14vLyQvBxXJf60w_-n1dPFhqV-W6bjWwbw&s",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN14vLyQvBxXJf60w_-n1dPFhqV-W6bjWwbw&s",
     title: "Festival Creatives",
-    pdfUrl: ""
+    pdfUrl: "",
   },
- 
-  
-] 
+];
 
 export const getCommChecker: GridColDef[] = [
   {
@@ -2002,4 +2003,75 @@ export const department = [
 export const TypeOfDocuments = [
   { value: "Circular", label: "Circular" },
   { value: "SEBI", label: "SEBI" },
+];
+
+export const InfoCapsules = [
+  {
+    main: "NSE",
+    segments: [
+      { id: 1, label: "Equity", status: "Active" },
+      { id: 2, label: "F & O", status: "Active" },
+      { id: 3, label: "Currency", status: "Active" },
+      { id: 4, label: "Commodity", status: "Inactive" },
+    ],
+  },
+  {
+    main: "BSE",
+    segments: [
+      { id: 1, label: "Equity", status: "Active" },
+      { id: 2, label: "F & O", status: "Active" },
+      { id: 3, label: "Currency", status: "Active" },
+    ],
+  },
+  {
+    main: "MCX",
+    segments: [{ id: 4, label: "Commodity", status: "Inactive" }],
+  },
+];
+export const information = [
+  {
+    id: 1,
+    heading: "Equity Delivery",
+    info: "(Subject to Minimum Retention of 0.5% of turnover)",
+  },
+  {
+    id: 2,
+    heading: "Equity Intraday",
+    info: "(Subject to Minimum Retention of 0.5% of turnover)",
+  },
+  {
+    id: 3,
+    heading: "Equity Futures",
+    info: "(Subject to Minimum Retention of 0.5% of turnover)",
+  },
+  {
+    id: 4,
+    heading: "Equity Options",
+    info: "(Subject to Minimum Retention of 0.5% of turnover)",
+  },
+  {
+    id: 5,
+    heading: "Currency Futures",
+    info: "(Subject to Minimum Retention of 0.5% of turnover)",
+  },
+  {
+    id: 6,
+    heading: "Currency Options",
+    info: "(Subject to Minimum Retention of 0.5% of turnover)",
+  },
+  {
+    id: 7,
+    heading: "Commodity Futures",
+    info: "(Subject to Minimum Retention of 0.5% of turnover)",
+  },
+  {
+    id: 8,
+    heading: "Commodity Options",
+    info: "(Subject to Minimum Retention of 0.5% of turnover)",
+  },
+  {
+    id: 9,
+    heading: "SLBM",
+    info: "(Subject to Minimum Retention of 0.5% of turnover)",
+  },
 ];
---
+++ src/pages/refCard/Registration Details/index.tsx
@@ -0,0 +1,295 @@
+import { Card, CardHeader, CardBody } from "reactstrap";
+import { TextField, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
+import { InfoCapsules, information } from "../../../helper/tableColumns.tsx";
+
+const RegisDetails = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <>
+      {/* Business Dashboard Card */}
+      <Card>
+        <CardHeader>
+          <h4 className="card-title mb-0">Registration Details</h4>
+        </CardHeader>
+        <CardBody>
+          {/* AP Name */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              marginBottom: "1rem",
+            }}
+          >
+            <Typography style={{ marginRight: "8px" }}>AP Name :</Typography>
+            <TextField variant="standard" sx={{ width: "60ch" }} />
+          </div>
+
+          {/* AP Registration Number */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              marginBottom: "1rem",
+            }}
+          >
+            <Typography style={{ marginRight: "8px" }}>
+              AP Registration Number :
+            </Typography>
+            <TextField variant="standard" sx={{ width: "48ch" }} />
+          </div>
+
+          {/* Date of Registration */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              marginBottom: "1rem",
+            }}
+          >
+            <Typography style={{ marginRight: "8px" }}>
+              Date of Registration :
+            </Typography>
+            <TextField variant="standard" sx={{ width: "52ch" }} />
+          </div>
+
+          {/* Registration Office */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              marginBottom: "1rem",
+            }}
+          >
+            <Typography style={{ marginRight: "8px" }}>
+              Registration Office :
+            </Typography>
+            <TextField variant="standard" sx={{ width: "53ch" }} />
+          </div>
+        </CardBody>
+      </Card>
+
+      {/* Segment Status Card */}
+      <Card>
+        <CardHeader>
+          <h5>Segment Status</h5>
+        </CardHeader>
+        <CardBody>
+          {InfoCapsules.map((exchange) => (
+            <div
+              key={exchange.main}
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: "40px",
+                alignItems: "stretch",
+                marginBottom: "20px",
+                flexDirection: isMobile ? "column" : "row",
+              }}
+            >
+              {/* Exchange Name (e.g., NSE, BSE) */}
+              <span
+                style={{
+                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.4)",
+                  backgroundColor: "#11395C",
+                  borderRadius: "23px",
+                  color: "white",
+                  padding: "10px 50px",
+                  border: "1px solid gray",
+                  display: "flex",
+                  alignItems: "center",
+                  fontSize: "20px",
+                  fontFamily: "Public Sans, sans-serif",
+                  fontWeight: 900,
+                  justifyContent: "center",
+                }}
+              >
+                {exchange.main}
+              </span>
+
+              {exchange.segments.map((segment) => (
+                <span
+                  key={segment.id}
+                  style={{
+                    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.4)",
+                    borderRadius: "23px",
+                    width: !isMobile ? "120px" : "",
+                    height: "60px",
+                    padding: "10px",
+                    border: "1px solid gray",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  {segment.label}
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "5px",
+                    }}
+                  >
+                    <span
+                      style={{
+                        width: "10px",
+                        height: "10px",
+                        borderRadius: "50%",
+                        backgroundColor:
+                          segment.status === "Active" ? "#01D28E" : "#ff0606",
+                      }}
+                    ></span>
+                    <span
+                      style={{
+                        fontSize: "12px",
+                        color: "gray",
+                      }}
+                    >
+                      {segment.status}
+                    </span>
+                  </div>
+                </span>
+              ))}
+            </div>
+          ))}
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <h5>No. of Terminal: 2</h5>
+        </CardHeader>
+        <CardBody>
+          <div
+            style={{
+              display: isMobile ? "" : "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            {/* Terminal 1 Details */}
+            <div style={{ width: isMobile ? "100%" : "48%" }}>
+              <h4>Terminal 1 Details</h4>
+              <div style={{ margin: "1rem 0" }}>
+                <Typography style={{ marginBottom: "5px" }}>
+                  Terminal Id:
+                </Typography>
+                <TextField variant="standard" fullWidth />
+              </div>
+              <div style={{ marginBottom: "1rem" }}>
+                <Typography style={{ marginBottom: "5px" }}>
+                  Terminal User:
+                </Typography>
+                <TextField variant="standard" fullWidth />
+              </div>
+              <div style={{ marginBottom: "1rem" }}>
+                <Typography style={{ marginBottom: "5px" }}>
+                  NISM Certificate No:
+                </Typography>
+                <TextField variant="standard" fullWidth />
+              </div>
+              <div style={{ marginBottom: "1rem" }}>
+                <Typography style={{ marginBottom: "5px" }}>
+                  NISM Certificate Valid till:
+                </Typography>
+                <TextField variant="standard" fullWidth />
+              </div>
+            </div>
+
+            <div style={{ width: isMobile ? "100%" : "48%" }}>
+              <h4>Terminal 2 Details</h4>
+              <div style={{ margin: "1rem 0" }}>
+                <Typography style={{ marginBottom: "5px" }}>
+                  Terminal Id:
+                </Typography>
+                <TextField variant="standard" fullWidth />
+              </div>
+              <div style={{ marginBottom: "1rem" }}>
+                <Typography style={{ marginBottom: "5px" }}>
+                  Terminal User:
+                </Typography>
+                <TextField variant="standard" fullWidth />
+              </div>
+              <div style={{ marginBottom: "1rem" }}>
+                <Typography style={{ marginBottom: "5px" }}>
+                  NISM Certificate No:
+                </Typography>
+                <TextField variant="standard" fullWidth />
+              </div>
+              <div style={{ marginBottom: "1rem" }}>
+                <Typography style={{ marginBottom: "5px" }}>
+                  NISM Certificate Valid till:
+                </Typography>
+                <TextField variant="standard" fullWidth />
+              </div>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <h5>Revenue Share of AP - 70%</h5>
+        </CardHeader>
+        <CardBody>
+          <div
+            style={{
+              display: isMobile ? "" : "grid",
+
+              gridTemplateColumns: "repeat(3, 1fr)",
+              gap: "10px",
+              padding: isMobile ? "0" : "10px",
+            }}
+          >
+            {information.map((item) => (
+              <span
+                key={item.id}
+                style={{
+                  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.4)",
+                  borderRadius: "23px",
+                  height: "60px",
+                  padding: "10px",
+                  border: "1px solid gray",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginBottom: isMobile ? "24px" : "0",
+                }}
+              >
+                {item.heading} - 70%
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "5px",
+                  }}
+                >
+                  <span
+                    style={{
+                      width: "10px",
+                      height: "10px",
+                      borderRadius: "50%",
+                    }}
+                  ></span>
+                  <span
+                    style={{
+                      fontSize: "12px",
+                      color: "gray",
+                    }}
+                  >
+                    {item.info}
+                  </span>
+                </div>
+              </span>
+            ))}
+          </div>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default RegisDetails;
