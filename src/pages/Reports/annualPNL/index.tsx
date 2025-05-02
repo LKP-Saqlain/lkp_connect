b1f8ee4babf1e0552e1da3de7638eae6bc4efee4
RahulMaurya <rahul1@lkpsec.com>
2025-05-02 14:57:31 +0530

    pnl final changes added

--- src/pages/Reports/annualPNL/index.tsx
+++ src/pages/Reports/annualPNL/index.tsx
@@ -21,8 +21,8 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import ShowToast from "../../../utils/toastUtils";
 
 const financialYears = [
-  { value: "2023-2024", label: "2023-2024" },
-  // { value: "2024-2025", label: "2024-2025" },
+  // { value: "2023-2024", label: "2023-2024" },
+  { value: "2024-2025", label: "2024-2025" },
 ];
 
 const AnnualPNL = () => {
