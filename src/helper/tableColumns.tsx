3db6af8f3076eeadf5807edb4b5cc4b3cdd37a9e
RahulMaurya <rahul1@lkpsec.com>
2025-05-22 14:40:51 +0530

    changes added

--- src/helper/tableColumns.tsx
+++ src/helper/tableColumns.tsx
@@ -3823,6 +3823,14 @@ export const preTradeColumns: GridColDef[] = [
     align: "center",
   },
   {
+    field: "status",
+    headerName: "Status",
+    width: 100,
+    disableColumnMenu: true,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
     field: "clientCode",
     headerName: "ClientCode",
     width: 100,
@@ -3925,14 +3933,6 @@ export const preTradeColumns: GridColDef[] = [
   {
     field: "remarks",
     headerName: "Remark",
-    width: 160,
-    disableColumnMenu: true,
-    headerAlign: "center",
-    align: "center",
-  },
-  {
-    field: "status",
-    headerName: "Status",
     width: 160,
     disableColumnMenu: true,
     headerAlign: "center",
