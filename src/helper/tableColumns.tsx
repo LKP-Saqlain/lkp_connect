7961f70bc1019c322ebb40f4b7c14161bd36808c
RahulMaurya <rahul1@lkpsec.com>
2025-08-25 11:54:02 +0530

    hashing changes added

--- src/helper/tableColumns.tsx
+++ src/helper/tableColumns.tsx
@@ -787,15 +787,23 @@ export const slbmColumns: GridColDef[] = [
   {
     field: "rmName",
     headerName: "RM Name",
-    minWidth: 120,
+    minWidth: 260,
     flex: 1,
     disableColumnMenu: true,
     headerClassName: "header-wrap-custom",
   },
   {
     field: "dealerName",
-    headerName: "Dealer Name",
-    minWidth: 120,
+    headerName: "Dealer Namess",
+    minWidth: 260,
+    flex: 1,
+    disableColumnMenu: true,
+    headerClassName: "header-wrap-custom",
+  },
+  {
+    field: "apName",
+    headerName: "AP Name",
+    minWidth: 260,
     flex: 1,
     disableColumnMenu: true,
     headerClassName: "header-wrap-custom",
@@ -6386,6 +6394,57 @@ export const ClientPledgeRequest: GridColDef[] = [
         maximumFractionDigits: 2,
       }).format(value);
     },
+  },
+  {
+    field: "ledgerReport",
+    headerName: "Client Report with Ledger",
+    headerClassName: "header-wrap-custom",
+    minWidth: 120,
+    flex: 1,
+    align: "right",
+    headerAlign: "center",
+    disableColumnMenu: true,
+    // valueFormatter: (params: any) => {
+    //   const value = parseFloat(params);
+    //   return new Intl.NumberFormat("en-IN", {
+    //     minimumFractionDigits: 2,
+    //     maximumFractionDigits: 2,
+    //   }).format(value);
+    // },
+  },
+  {
+    field: "pledgeReport",
+    headerName: "Client Report with Pledge",
+    headerClassName: "header-wrap-custom",
+    minWidth: 120,
+    flex: 1,
+    align: "right",
+    headerAlign: "center",
+    disableColumnMenu: true,
+    // valueFormatter: (params: any) => {
+    //   const value = parseFloat(params);
+    //   return new Intl.NumberFormat("en-IN", {
+    //     minimumFractionDigits: 2,
+    //     maximumFractionDigits: 2,
+    //   }).format(value);
+    // },
+  },
+  {
+    field: "freeHolding Value",
+    headerName: "Free Holding Value",
+    headerClassName: "header-wrap-custom",
+    minWidth: 120,
+    flex: 1,
+    align: "right",
+    headerAlign: "center",
+    disableColumnMenu: true,
+    // valueFormatter: (params: any) => {
+    //   const value = parseFloat(params);
+    //   return new Intl.NumberFormat("en-IN", {
+    //     minimumFractionDigits: 2,
+    //     maximumFractionDigits: 2,
+    //   }).format(value);
+    // },
   },
   {
     field: "encryptedCode",
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    manifest: true, // optional, helpful for advanced setups
+    rollupOptions: {
+      output: {
+        // hashed filenames (default in Vite)
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
+      },
+    },
+  },
+});
