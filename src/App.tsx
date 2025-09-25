eec23c48ed01f0161791c65566725b57294835b2
RahulMaurya <rahul1@lkpsec.com>
2025-09-25 12:38:44 +0530

    mandate code manually added

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,8 @@ const ForgotPassword = lazy(
 );
 const SideBar = lazy(() => import("./components/sideBar"));
 
+const DpMandate = lazy(() => import("./pages/Masters/MandateCall"));
+
 const App = () => {
   const [serverOnline, setServerOnline] = useState(true);
   const [wasOffline, setWasOffline] = useState(false);
@@ -77,6 +79,18 @@ const App = () => {
             path="/dashboard"
             element={
               <PrivateRoute customLogin={false} dashElement={<SideBar />} />
+            }
+          />
+          <Route
+            path="/DPMandate"
+            element={
+              <PrivateRoute customLogin={false} dashElement={<DpMandate />} />
+            }
+          />
+          <Route
+            path="/DPMandate/:encryptedCode"
+            element={
+              <PrivateRoute customLogin={false} dashElement={<DpMandate />} />
             }
           />
         </Routes>
