1cfb6d0c836b22a4c7f773888faa5628d9124e71
RahulMaurya <rahul1@lkpsec.com>
2025-11-18 14:34:36 +0530

    changes added

--- src/pages/Reports/SLBM/index.tsx
+++ src/pages/Reports/SLBM/index.tsx
@@ -60,7 +60,7 @@ const SlbmHoling = ({ activeSubItem }: any) => {
     selectedBranchCode: Yup.object()
       .nullable()
       .required("Branch code is required"),
-    // isInValue: Yup.string().required("SYMBOL / ISIN is required"),
+    isInValue: Yup.string().required("SYMBOL / ISIN is required"),
   });
 
   interface FormValues {
@@ -614,7 +614,7 @@ const SlbmHoling = ({ activeSubItem }: any) => {
                                 formik.errors.selectedBranchCode) ||
                               (formik.touched.isInValue &&
                                 formik.errors.isInValue)
-                                ? "-18px"
+                                ? "-25px"
                                 : "",
                           }}
                         >
